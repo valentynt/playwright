dad05d5c98a7c0e6bb773abb46c281318f8ea80b
Valentyn Tyrtychko <valentyntyrtychko@gmail.com>
2024-09-15 15:20:24 +0300

    playwright homework5

--- playwright.config.js
+++ playwright.config.js
@@ -5,11 +5,11 @@ dotenv.config(); // Загрузка переменных окружения
 
 export default defineConfig({
   use: {
-    baseURL: process.env.BASE_URL || "https://default-url.com",
+    baseURL: process.env.BASE_URL,
 
     httpCredentials: {
-      username: process.env.HTTP_USERNAME || "default-username",
-      password: process.env.HTTP_PASSWORD || "default-password",
+      username: process.env.HTTP_USERNAME,
+      password: process.env.HTTP_PASSWORD,
     },
 
     // Остальные настройки
