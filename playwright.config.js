be9ede90db2c50ddf93a21c7a12cf0e63d40a6bf
Valentyn Tyrtychko <valentyntyrtychko@gmail.com>
2024-10-02 22:59:40 +0300

    fixed error

--- playwright.config.js
+++ playwright.config.js
@@ -7,11 +7,6 @@ export default defineConfig({
   use: {
     baseURL: process.env.BASE_URL,
 
-    httpCredentials: {
-      username: process.env.HTTP_USERNAME,
-      password: process.env.HTTP_PASSWORD,
-    },
-
     // Остальные настройки
     browserName: "chromium",
     viewport: { width: 1280, height: 720 },
